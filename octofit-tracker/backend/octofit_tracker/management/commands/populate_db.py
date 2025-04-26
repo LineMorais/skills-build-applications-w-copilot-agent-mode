476172b7f967f3c7b718f139af3ccbd61c387b6f
LineMorais <107950543+LineMorais@users.noreply.github.com>
2025-04-26 14:57:15 +0000

    Setup database structure, Django project updates, and populate database with test data

---
+++ octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
@@ -0,0 +1,31 @@
+from django.core.management.base import BaseCommand
+from octofit_tracker.tracker.models import User, Team, Activity, Leaderboard, Workout
+from pymongo import MongoClient
+from datetime import timedelta
+from bson import ObjectId
+from django.conf import settings
+from octofit_tracker.test_data import test_data
+
+class Command(BaseCommand):
+    help = 'Populate the database with test data for users, teams, activity, leaderboard, and workouts'
+
+    def handle(self, *args, **kwargs):
+        # Connect to MongoDB
+        client = MongoClient(settings.DATABASES['default']['HOST'], settings.DATABASES['default']['PORT'])
+        db = client[settings.DATABASES['default']['NAME']]
+
+        # Drop existing collections
+        db.users.drop()
+        db.teams.drop()
+        db.activity.drop()
+        db.leaderboard.drop()
+        db.workouts.drop()
+
+        # Insert test data
+        db.users.insert_many(test_data['users'])
+        db.teams.insert_many(test_data['teams'])
+        db.activity.insert_many(test_data['activities'])
+        db.leaderboard.insert_many(test_data['leaderboard'])
+        db.workouts.insert_many(test_data['workouts'])
+
+        self.stdout.write(self.style.SUCCESS('Successfully populated the database with test data.'))
